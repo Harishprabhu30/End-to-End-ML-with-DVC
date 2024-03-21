ee6cbda1806093bfb461de37fffb4402670148b0
Harish Prabhu <111897267+Harishprabhu30@users.noreply.github.com>
2024-03-21 21:32:13 +0530

    Requirements Added

--- template.py
+++ template.py
@@ -9,20 +9,21 @@ project_name = 'CNN_Classifier'
 list_of_files = [
 
     '.github/workflows/.gitkeep',
-    f"src{project_name}/__init__.py",
-    f"src{project_name}/components/__init__.py",
-    f"src{project_name}/utils/__init__.py",
-    f"src{project_name}/config/__init__.py",
-    f"src{project_name}/config/configuration.py",
-    f"src{project_name}/pipeline/__init__.py",
-    f"src{project_name}/entity/__init__.py",
-    f"src{project_name}/constant/__init__.py",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constant/__init__.py",
     "config/config.yaml",
     'dvc.yaml',
     'params.yaml',
     'requirements.txt',
     'setup.py',
-    'research/trails.ipynb'
+    'research/trails.ipynb',
+    'templates/index.html'
 
 ]
 
