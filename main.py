357c6b860e711988bcdec66789a466130e2aa2da
Harish Prabhu <111897267+Harishprabhu30@users.noreply.github.com>
2024-03-22 19:38:32 +0530

    Base Model Added
    Stage 02 is completed: Created Base Model VGG16

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 from CNN_Classifier import logger
+from CNN_Classifier.pipeline.stage_01_data_ingestion import *
+from CNN_Classifier.pipeline.stage_02_prepare_base_model import *
 
-logger.info('Welcome to my Custom Log')
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx========================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+###############################################################################################################
+
+STAGE_NAME = "Prepare Base Model"
+
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = PrepareBaseModelTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx========================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
